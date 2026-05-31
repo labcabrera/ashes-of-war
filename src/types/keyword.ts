f73bea25854f2b309c4376081a4436f4d1a65423
labcabrera <lab.cabrera@gmail.com>
2026-05-31 19:57:49 +0200

    Added units.

--- src/types/keyword.ts
+++ src/types/keyword.ts
@@ -1,11 +1,13 @@
 /** All valid rule keyword tags that can be assigned to unit roster entries. */
 export type UnitKeyword =
   | 'air-support'
+  | 'explosives'
   | 'half-track'
   | 'low-reliability'
   | 'medic'
   | 'open-topped'
   | 'radio'
+  | 'indirect-fire'
   | 'reconnaissance'
   | 'sniper'
   | `transport-${number}`;
